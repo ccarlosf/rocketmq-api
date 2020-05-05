4e10d39645fe51d5836ae0bbc1e52a34c324ce27
ccarlos <2238513100@qq.com>
2020-05-05 11:32:29 +0800

    rocketmq-api学习项目

---
+++ src/test/java/com/ccarlos/rocketmq/RocketmqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
