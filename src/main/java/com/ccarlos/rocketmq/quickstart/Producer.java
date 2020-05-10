2c20f5d0c2ae788b30a998262d66eb0a38009c4d
ccarlos <2238513100@qq.com>
2020-05-10 19:02:29 +0800

    3-3 四种集群构建模式讲解 3-4 主从集群模式搭建 3-5 主从集群模式下的高可用机制故障演练 3-6 本章小结 4-1 本章导航 4-2 核心配置参数讲解 4-3 主从同步机制分析-1 4-4 主从同步机制分析-2 4-5 消息同步发送机制分析-1 4-6 消息同步发送机制分析-2 4-7 netty通信机制分析 4-8 消息的返回状态讲解 4-9 消息的延迟投递讲解

--- src/main/java/com/ccarlos/rocketmq/quickstart/Producer.java
+++ src/main/java/com/ccarlos/rocketmq/quickstart/Producer.java
@@ -3,10 +3,7 @@ package com.ccarlos.rocketmq.quickstart;
 import com.ccarlos.rocketmq.constants.Const;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.client.producer.MessageQueueSelector;
-import org.apache.rocketmq.client.producer.SendResult;
-import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.client.producer.*;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.exception.RemotingException;
@@ -15,33 +12,53 @@ import java.util.List;
 
 public class Producer {
 
-	public static void main(String[] args) throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
+    public static void main(String[] args) throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
 
-		DefaultMQProducer producer = new DefaultMQProducer
-				("test_quick_producer_name");
+        DefaultMQProducer producer = new DefaultMQProducer
+                ("test_quick_producer_name");
 
 //		producer.setNamesrvAddr(Const.NAMESRV_ADDR_SINGLE);
 
-		producer.setNamesrvAddr(Const.NAMESRV_ADDR_MASTER_SLAVE);
+        producer.setNamesrvAddr(Const.NAMESRV_ADDR_MASTER_SLAVE);
 
-		// 手动设置超时时间 非常重要
-		producer.setSendMsgTimeout(10000);
+        // 手动设置超时时间 非常重要
+        producer.setSendMsgTimeout(10000);
 
-		producer.start();
+        producer.start();
 
-		for(int i = 0 ; i <1 ; i ++) {
-			//	1.	创建消息
-			Message message = new Message("test_quick_topic",    //	主题
-					"TagA", //	标签
-					"key" + i,    // 	用户自定义的key ,唯一的标识
-					("Hello RocketMQ" + i).getBytes());    //	消息内容实体（byte[]）
+        for (int i = 0; i < 2; i++) {
+            //	1.	创建消息
+            Message message = new Message("test_quick_topic",    //	主题
+                    "TagA", //	标签
+                    "key" + i,    // 	用户自定义的key ,唯一的标识
+                    ("Hello RocketMQ" + i).getBytes());    //	消息内容实体（byte[]）
 
-			SendResult sr = producer.send(message);
-			SendStatus status = sr.getSendStatus();
-			System.err.println(status);
-			System.err.println("消息发出: " + sr);
-		}
-		producer.shutdown();
+            // 设置延迟时间
+			if(i == 1) {
+				message.setDelayTimeLevel(3);
+			}
 
-	}
+            // 2.1 同步发送消息
+            SendResult sr = producer.send(message);
+            SendStatus status = sr.getSendStatus();
+            System.err.println(status);
+            System.err.println("消息发出: " + sr);
+
+            // 2.2 异步发送消息
+         /*   producer.send(message, new SendCallback() {
+                //rabbitmq急速入门的实战: 可靠性消息投递
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    System.err.println("msgId: " + sendResult.getMsgId() + ", status: " + sendResult.getSendStatus());
+                }
+
+                @Override
+                public void onException(Throwable e) {
+                    e.printStackTrace();
+                    System.err.println("------发送失败");
+                }
+            });*/
+        }
+        producer.shutdown();
+    }
 }
